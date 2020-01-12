0c64947b9baba9e0b375b650d2515956680d1794
Gabriel <gabriel.glitch2005@gmail.com>
2020-01-12 15:21:51 -0200

    getting ready for prod

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 
 //Set server
@@ -6,6 +7,9 @@ const app = express();
 
 //Bodyparser Middleware
 app.use(express.json());
+
+//Static files
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 //Use Routes
 const postsRouter = require('./routes/api/posts');
@@ -21,10 +25,8 @@ const homeRouter = require('./routes/api/home/home');
 app.use('/api/home', homeRouter);
 
 //Greetings
-app.get('/', (req, res) => {
-	res.send(
-		`Hey there! Welcome to the Anonymous Posts API! Check https://localhost:${port}/api/home to know our API!`
-	);
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
 });
 
 module.exports = app;
